75792d1dae92d5c3fe57e517169b327b0a3ee7df
Шабдан Таалайбек уулу <wunknown2@gmail.com>
2024-02-15 12:07:35 +0600

    ddd

--- src/components/movies/MovieCard.jsx
+++ src/components/movies/MovieCard.jsx
@@ -8,63 +8,68 @@ import {
   CardContent,
   CardMedia,
   IconButton,
-  Rating,
-  Stack,
   Typography,
 } from "@mui/material";
-import { AddShoppingCart, Details } from "@mui/icons-material";
+import { AddReaction } from "@mui/icons-material";
 import Detail from "./Detail";
 
 const MovieCard = ({ elem }) => {
   const { deleteMovie } = useMovies();
-  const [open, setOpen] = useState(false);
   const navigate = useNavigate();
+  const [open, setOpen] = useState(false);
+
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+
   return (
     <Card
       sx={{
-        height: 450,
+        height: 600,
         boxShadow: "none",
         margin: "2%",
         width: { md: "30vw", lg: "19vw" },
       }}
     >
       <CardActionArea onClick={handleOpen}>
-        <CardMedia sx={{ height: 240, borderRadius: 4 }} image={elem.image} />
+        <CardMedia
+          component="img"
+          image={elem.image}
+          alt={elem.title}
+          sx={{ height: 400, borderRadius: 4 }}
+        />
       </CardActionArea>
       <CardContent sx={{ padding: "20px 5px 0px 5px" }}>
-        <Typography variant="h5" fontSize="20" fontWeight={700} component="div">
+        <Typography variant="h5" fontSize="24" fontWeight={700} component="div">
           {elem.title}
         </Typography>
-        <Typography color="black" fontSize="24px" fontWeight={700}>
-          {elem.price}$
-        </Typography>
-        <Typography color="black" fontSize="24px" fontWeight={700}>
-          {elem.description}
-        </Typography>
-        <Typography color="black" fontSize="24px" fontWeight={700}>
-          {elem.category}
-        </Typography>
-        <Button
-          onClick={() => deleteMovie(elem.id)}
-          color="secondary"
-          variant="outlined"
-          size="medium"
-        >
-          Delete
-        </Button>
-        <Button
-          onClick={() => navigate(`/edit/${elem.id}`)}
-          color="primary"
-          variant="outlined"
-          size="medium"
-        >
-          Edit
-        </Button>
-        <IconButton>
-          <AddShoppingCart />
-        </IconButton>
+        {!open && (
+          <>
+            <Typography color="black" fontSize="15px" fontWeight={700}>
+              {elem.price} сом
+            </Typography>
+
+            <Button
+              onClick={() => navigate(`/edit/${elem.id}`)}
+              color="primary"
+              variant="outlined"
+              size="medium"
+            >
+              Редактировать
+            </Button>
+            <Button
+              onClick={() => deleteMovie(elem.id)}
+              color="secondary"
+              variant="outlined"
+              size="medium"
+            >
+              Удалить
+            </Button>
+
+            <IconButton>
+              <AddReaction />
+            </IconButton>
+          </>
+        )}
       </CardContent>
       <Detail open={open} handleClose={handleClose} elem={elem} />
     </Card>
